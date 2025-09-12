cb15b883a85f32b1db6c92deac0ee631d80019c2
longhoangk67hus <hoangngoclong_t67@hus.edu.vn>
2025-09-13 00:13:34 +0700

    Init Create Default Packages

---
+++ Editor/ToolsMenu.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using static System.IO.Directory;
+using static System.IO.Path;
+using static UnityEditor.AssetDatabase;
+using static UnityEngine.Application;
+
+namespace LH
+{
+    public static class ToolsMenu 
+    {
+        [MenuItem("Tools/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+            Dir("_Game", "Scenes", "Scripts", "Prefabs", "ScriptableObjects", "Graphics", "Audio", "Animations",  "Resources");
+            Dir();
+            Refresh();
+        }
+        public static void Dir(string root, params string[] dirs)
+        {
+            var fullPath = Combine(dataPath, root);
+            foreach (var newDirectory in dirs)
+            {
+                CreateDirectory(Combine(fullPath, newDirectory));
+            }
+        }
+    }
+    
+    
+}
+ 
