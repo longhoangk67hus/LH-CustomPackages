82e5d7b125277abd842d4284178f9e8255cba8ec
longhoangk67hus <hoangngoclong_t67@hus.edu.vn>
2025-09-15 20:39:43 +0700

    update singleton file

--- Packages/LH.CreateDefaultDirectory/Editor/ToolsMenu.cs
+++ Packages/LH.CreateDefaultDirectory/Editor/ToolsMenu.cs
@@ -6,12 +6,12 @@ using static UnityEngine.Application;
 
 namespace LH
 {
-    public static class ToolsMenu 
+    public static class ToolsMenu
     {
         [MenuItem("Tools/Setup/Create Default Folders")]
         public static void CreateDefaultFolders()
         {
-            Dir("_Game", "Scenes", "Scripts", "Prefabs", "ScriptableObjects", "Graphics", "Audio", "Animations",  "Resources", "Sprites");
+            Dir("_Game", "Scenes", "Scripts", "Prefabs", "ScriptableObjects", "Graphics", "Audio", "Animations", "Resources", "Sprites");
             Dir("Scripts", "Singleton", "Manager");
             Refresh();
         }
@@ -23,8 +23,17 @@ namespace LH
                 CreateDirectory(Combine(fullPath, newDirectory));
             }
         }
+        private static void CreateSingletonScript()
+        {
+            string scriptsPath = Combine(dataPath, "Scripts", "Singleton");
+            string targetFile = Combine(scriptsPath, "Singleton.cs");
+            string templatePath = Combine("Packages", "LH.CreateDefaultDirectory", "Editor", "Singleton.cs.txt");
+
+            if (!File.Exists(targetFile) && File.Exists(templatePath))
+            {
+                File.Copy(templatePath, targetFile);
+            }
+        }
     }
     
-    
 }
- 
